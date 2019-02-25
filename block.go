a54c6dfc6a668c7f397b3de769fdc56bbd4e2160
Matthew Hartstonge <matt@mykro.co.nz>
2019-02-26 00:24:09 +1300

    :arrow_up: block: implements data structure.

--- block.go
+++ block.go
@@ -2,11 +2,25 @@ package capi
 
 // Block provides the domain model for a blockchain block.
 type Block struct {
-	ID            string `json:"id"     storm:"id"`
-	Height        int    `json:"height" storm:"index"`
-	Hash          string `json:"hash"   storm:"index"`
-	Confirmations int64  `json:"confirmations"`
-	Size          int32  `json:"size"`
+	ID                string   `json:"id" storm:"id"`
+	Hash              string   `json:"hash" storm:"index"`
+	Confirmations     int      `json:"confirmations"`
+	StrippedSize      int      `json:"strippedsize"`
+	Size              int      `json:"size"`
+	Weight            int      `json:"weight"`
+	Height            int      `json:"height" storm:"index"`
+	Version           int      `json:"version"`
+	VersionHex        string   `json:"versionHex"`
+	MerkleRoot        string   `json:"merkleroot"`
+	BlockTransactions []string `json:"tx"`
+	Time              int64    `json:"time"`
+	Mediantime        int      `json:"mediantime"`
+	Nonce             uint32   `json:"nonce"`
+	Bits              string   `json:"bits"`
+	Difficulty        float64  `json:"difficulty"`
+	Chainwork         string   `json:"chainwork"`
+	PreviousHash      string   `json:"previousBlockHash"`
+	NextHash          string   `json:"nextBlockHash"`
 }
 
 // TODO: Block methods go here...
